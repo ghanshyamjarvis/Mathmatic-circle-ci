ca3745733f90586a84de8677bbf5a22dcc9654fd
ghanshyamjarvis <ghanshyam.jarvis@gmail.com>
2020-07-27 17:20:24 +0530

    add one test case

--- app.js
+++ app.js
@@ -1,34 +1,8 @@
 var calculator = require('./calculator');
+
 var a=10, b=5;
 
 
 console.log("Addition : "+calculator.add(a,b));
 
-
-
-
-
-/*const mysql = require('mysql');
-const express = require('express');
-const app = express();
-
-//create connection
-const db = mysql.createConnection({
-  host: '127.0.0.1',
-  user:'root',
-  password: 'password',
-  database: 'Emp'
-});
-
-app.get('/all',(req,res) => {
-  const sql = `select * from emp`;
-  const query = db.query(sql,(err,results) => {
-    if (err) throw err;
-    res.json({data:results})
-  })
-});
-
-
-app.listen(3000, ()=>{
-  console.log('server start on 3000 port')
-});*/
+console.log("Subtraction : "+calculator.subtract(a,b));
